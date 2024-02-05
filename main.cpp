51fe64d117029ae7fffb33cd12af12a111c6c6ae
Valgin <62236015+Valgin@users.noreply.github.com>
2024-02-05 13:43:19 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "lib.h"
+//aaaa
+#include <iostream>
+
+int main (int, char **) {
+    std::cout << "Version: " << version() << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
